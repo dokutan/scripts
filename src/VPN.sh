4c7fd8b2e67228089588a5b5a1a6c7046c2c62a4
dokutan <54861821+dokutan@users.noreply.github.com>
2020-11-23 20:02:55 +0100

    Add src/VPN.sh

---
+++ src/VPN.sh
@@ -0,0 +1,17 @@
+#!/bin/env sh
+# A wrapper around openpyn for NordVPN connections.
+# Disables NetworkManager to prevent DNS leaks.
+# Usage:
+# VPN start [2-letter country code]
+# ^C && VPN stop
+if [ "$1" = "start" ]
+then
+	systemctl stop NetworkManager.service
+	openpyn -f $2 --p2p
+elif [ "$1" = "stop" ]
+then
+	openpyn -x
+	systemctl start NetworkManager.service
+else
+	echo "unknown action, expected start or stop"
+fi

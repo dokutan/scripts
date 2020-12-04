338b240e00a6809c0137444eb14e6fff163f3941
dokutan <54861821+dokutan@users.noreply.github.com>
2020-12-04 08:55:50 +0100

    Add screenlock.sh

---
+++ src/screenlock.sh
@@ -0,0 +1,11 @@
+#!/bin/env sh
+# turn display off, disable console switching, lock
+# enable console switching after unlock
+
+# lockscreen image
+IMAGE="/path/to/image.png"
+
+xset dpms force off
+physlock -l
+i3lock -n -i $IMAGE
+physlock -L

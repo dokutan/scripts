59b619b10ca056657b804735084115724e3569b8
dokutan <54861821+dokutan@users.noreply.github.com>
2020-12-23 17:09:39 +0100

    Add keyboard-color.sh

---
+++ src/keyboard-color.sh
@@ -0,0 +1,14 @@
+#!/bin/env sh
+# GUI to change the LED color on keyboards supported by https://github.com/dokutan/rgb_keyboard
+
+# get color from user
+values=$(zenity --color-selection --show-palette | sed 's/[rgb()]//g' | tr "," "\n")
+
+# convert decimal rgb values to one hexadecimal rgb value
+for c in $values
+do
+	d="$d"$(printf "%02x" "$c")
+done
+
+# echo "$d"
+rgb_keyboard --color "$d"

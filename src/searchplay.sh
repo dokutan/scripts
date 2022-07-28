dcfd811f9cbd4516146ec2d1ff17ee7e80b10fd6
dokutan <54861821+dokutan@users.noreply.github.com>
2022-07-28 17:21:38 +0200

    Update searchplay

--- src/searchplay.sh
+++ src/searchplay.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 # An interactive wrapper for `mpc searchplay`.
 
-[ -t 1 ] && MENU="fzf" || MENU="rofi -dmenu -i"
-mpc searchplay "$(mpc list title | $MENU)"
-
+[ -t 1 ] && MENU="fzf +s" || MENU="rofi -dmenu -i"
+RESULT="$(mpc playlist -f "%artist%\t%album%\t%track%\t%title%" | $MENU)" || exit 1
+mpc searchplay \
+	artist "$(echo "$RESULT" | cut -f 1)" \
+	album "$(echo "$RESULT" | cut -f 2)" \
+	title "$(echo "$RESULT" | cut -f 4)" 
